e51b251387afd356588bc9631d514ac29ee9a83b
Aidan Baack <aebaack@gmail.com>
2016-11-18 16:34:14 -0700

    Finished basic functionality for API.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-var 
+'use strict';
+
+const button = $('.submit-button');
+const search = $('.search-box');
+
+button.on('click', () => {
+  let searchQuery = search.val();
+  if (!searchQuery) {
+    return console.log('doesnt work');
+  }
+
+  $.getJSON(`http://lvh.me:8000/api/albums/${searchQuery}`, data => {
+    console.log(data);
+  });
+});
