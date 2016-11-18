e6091f010ce79c49fd4f536d8d90d2e29b643b7c
Aidan Baack <aebaack@gmail.com>
2016-11-18 16:12:51 -0700

    Add basic api functionality

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 'use strict';
 
 const env = 'development';
-const config = require('./knex.js')[env];
+const config = require('./js/knex.js')[env];
 const knex = require('knex')(config);
 
 const express = require('express');
 const app = express();
 
-app.get('/api/users', (req, res, next) => {
+app.get('/api/albums/:name', (req, res) => {
+  let album = req.params.name;
 
+  knex('albums')
+  .where('albums.name', album)
+  .then(data => {
+    // console.log(data);
+    res.send(data);
+    knex.destroy();
+    // process.exit(1);
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
+});
+
+app.listen('8000', () => {
+  console.log('Listening on port 8000');
 });
